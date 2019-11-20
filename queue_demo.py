c2c892fbb5dbf4bfdd56cb46bbb391eb4204dff1
wangmengmeng <wmm_0165@163.com>
2019-11-20 17:21:39 +0800

    增加使用队列发任务

---
+++ queue_demo.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/11/20 15:11
+# @Author : wangmengmeng
+import os
+import requests
+import time
+from common.record_log import log
+from concurrent.futures import ThreadPoolExecutor
+# 使用线程池
+import datetime
+import queue
+# python2中是Queue，python3中是queue
+
+class SendXml:
+    def __init__(self):
+        pass
+
+    def read_file(self, path):
+        files = os.listdir(path)
+        q = queue.Queue()
+        n = 0
+        for file in files:
+            with open(os.path.join(path, file), mode='r', encoding='utf-8', buffering=True) as f:
+                file_content = f.read()
+            q.put(file_content)
+            n += 1
+            print(n)
+            if n % 100 == 0:
+                time.sleep(3)
+                while not q.empty():
+                    with ThreadPoolExecutor(max_workers=2) as executor:
+                        for i in range(1):
+                            executor.submit(self.req(q.get()))
+
+    def req(self, content):
+        url = "http://10.1.1.71:9999/auditcenter/api/v1/auditcenter"
+        headers = {"Content-Type": "text/plain"}
+        res = requests.post(url, data=content.encode("utf-8"), headers=headers)
+        log.info("执行结果：{}".format(res.json()))
+        # return res
+
+
+if __name__ == '__main__':
+    sx = SendXml()
+    path = r'D:\data\2019-11-02\H0003\receive_path'
+    sx.read_file(path)
--- send_xml.py
+++ send_xml.py
@@ -3,7 +3,9 @@
 # @Author : wangmengmeng
 import os
 import requests
+import time
 from common.record_log import log
+import datetime
 
 
 class SendXml:
@@ -11,36 +13,29 @@ class SendXml:
         pass
 
     def send_xml(self, path):
+        start = datetime.datetime.now()
         url = "http://10.1.1.71:9999/auditcenter/api/v1/auditcenter"
         headers = {"Content-Type": "text/plain"}
         files = os.listdir(path)
+        n = 0
         for file in files:
+
             with open(os.path.join(path, file), mode='r', encoding='utf-8', buffering=True) as f:
                 file_content = f.read()
                 res = requests.post(url, data=file_content.encode("utf-8"), headers=headers)
                 log.info("执行结果：{}".format(res.json()))
 
+            n += 1
+            if n % 100 == 0:
+                time.sleep(10)
+        end = datetime.datetime.now()
+        print(end - start)
+
 
 if __name__ == '__main__':
     sx = SendXml()
-    paths = [r'C:\Users\iphauser\Desktop\data\2019-11-01\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-02\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-03\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-04\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-05\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-06\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-07\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-08\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-09\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-10\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-11\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-12\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-13\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-14\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-15\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-16\H0003\receive_path',
-             r'C:\Users\iphauser\Desktop\data\2019-11-17\H0003\receive_path', ]
+    paths = [r'C:\Users\iphauser\Desktop\data\2019-11-01\H0003\receive_path']
     # for path in paths:
     #     sx.send_xml(path)
-    path = r'D:\ccc'
+    path = r'D:\data\2019-11-02\H0003\receive_path'
     sx.send_xml(path)
